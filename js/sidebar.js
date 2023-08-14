4b400065c858e227d541a41763cc4595aa2c8a0c
zhiqieryi2 <2943187728@qq.com>
2023-08-15 02:48:49 +0800

    first commit

---
+++ js/sidebar.js
@@ -0,0 +1,11 @@
+// sidebar js
+function openNav() {
+    document.getElementById("mySidebar").style.width = "175px";
+    document.getElementById("main").style.marginLeft = "175px";
+}
+
+/* 设置侧边栏宽度为0，页面内容左边距为0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
